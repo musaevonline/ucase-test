fbb54bdf4e9d9a8bd360b27b5df84df0f42a783e
Amir <amir@musaev.online>
2020-12-10 17:03:09 +0000

    feat: v1

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,17 +28,23 @@ export default {
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss'
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
+    '@nuxtjs/apollo'
   ],
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
+  },
+  apollo: {
+    clientConfigs: {
+      default: {
+        httpEndpoint: 'https://api.graphql.jobs',
+      }
+    }
   }
 }
